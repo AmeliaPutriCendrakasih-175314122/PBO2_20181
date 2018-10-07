bd6d267f95274db77de297a7a3357a710324ccb6
CX <CX@CX25>
2018-10-08 02:36:19 +0700

    versi 0.sekian

--- src/model/AntrianPasien.java
+++ src/model/AntrianPasien.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 import java.util.ArrayList;
@@ -13,41 +12,66 @@ import java.util.ArrayList;
  * @author Kasih_175314122
  */
 public class AntrianPasien {
-     private int tanggalAntrian;//pendeklarasian variable tanggalAntrian dengan tipe integer.
-     private int bulanAntrian;//pendeklarasian variable bulanAntrian dengan tipe integer.
-     private int tahunAntrian; //pendeklarasian variable tahunAntrian dengan tipe integer.
-     private Klinik klinik; //pendeklarasian variable klinik dengan tipe klinik.
-     private Pasien daftarPasien[];//pendeklarasian variable array daftarpasien dengan tipe pasien. 
-     private ArrayList<Pasien> daftarPasienAntri = new ArrayList<Pasien>();//mendeklarasikan variabel array pasienList dengan tipe objek ArrayList
-     public static ArrayList<AntrianPasien> antrianPasien = new ArrayList<AntrianPasien>();
- /**
- * method getKlinik dengan tipe Klinik dan didalam method ini terdapat return Klinik dimana nilai dari nama akan dibalikan ke getKlinik
- */ 
+
+    private int tanggalAntrian;//pendeklarasian variable tanggalAntrian dengan tipe integer.
+    private int bulanAntrian;//pendeklarasian variable bulanAntrian dengan tipe integer.
+    private int tahunAntrian; //pendeklarasian variable tahunAntrian dengan tipe integer.
+    private Klinik klinik; //pendeklarasian variable klinik dengan tipe klinik.
+    private ArrayList<Pasien> daftarPasienAntri = new ArrayList<Pasien>();//mendeklarasikan variabel array pasienList dengan tipe objek ArrayList
+    public static ArrayList<AntrianPasien> antrianPasien = new ArrayList<AntrianPasien>();
+
+    public AntrianPasien() {
+    }
+
+    public AntrianPasien(int tanggalAntrian, int bulanAntrian, int tahunAntrian, Klinik klinik) {
+        this.tanggalAntrian = tanggalAntrian;
+        this.bulanAntrian = bulanAntrian;
+        this.tahunAntrian = tahunAntrian;
+        this.klinik = klinik;
+    }
+
+    /**
+     * method getKlinik dengan tipe Klinik dan didalam method ini terdapat
+     * return Klinik dimana nilai dari nama akan dibalikan ke getKlinik
+     */
     public Klinik getKlinik() {
         return klinik;
     }
-/**method setKlinik dengan tipe void dan berparameter Klinik klinik.
- * Didalam method terdapat this yang berfungsi untuk merujuk atribut dalam method yang sedang berlaku 
- * dan pada dasarnya this digunakan ketika nama atribut sama dengan variable local*/
+
+    /**
+     * method setKlinik dengan tipe void dan berparameter Klinik klinik. Didalam
+     * method terdapat this yang berfungsi untuk merujuk atribut dalam method
+     * yang sedang berlaku dan pada dasarnya this digunakan ketika nama atribut
+     * sama dengan variable local
+     */
     public void setKlinik(Klinik klinik) {
         this.klinik = klinik;
     }
-/**
- * method getDaftarPasien dengan tipe array Pasien dan didalam method ini terdapat return daftarpasien dimana nilai dari nama akan dibalikan ke getDaftarPasien
- */
-     public ArrayList<Pasien> getDaftarPasien() {
+
+    /**
+     * method getDaftarPasien dengan tipe array Pasien dan didalam method ini
+     * terdapat return daftarpasien dimana nilai dari nama akan dibalikan ke
+     * getDaftarPasien
+     */
+    public ArrayList<Pasien> getDaftarPasien() {
         return daftarPasienAntri;
     }
-/**method setDaftarPasien dengan tipe void dan berparameter Pasien[] daftarPasien.
- * Didalam method terdapat this yang berfungsi untuk merujuk atribut dalam method yang sedang berlaku 
- * dan pada dasarnya this digunakan ketika nama atribut sama dengan variable local*/
-    public void setDaftarPasien(ArrayList<Pasien> pasien) {
-        this.daftarPasienAntri = pasien;
+
+    /**
+     * method setDaftarPasien dengan tipe void dan berparameter Pasien[]
+     * daftarPasien. Didalam method terdapat this yang berfungsi untuk merujuk
+     * atribut dalam method yang sedang berlaku dan pada dasarnya this digunakan
+     * ketika nama atribut sama dengan variable local
+     */
+    public void setDaftarPasien(ArrayList<Pasien> daftarpasien) {
+        this.daftarPasienAntri = daftarpasien;
     }
+
     public void Mendaftar(Pasien pasien) {
         daftarPasienAntri.add(pasien);
     }
-     public static void daftarPasien(Pasien pasien, int tanggal, int bulan, int tahun, Klinik klinik) throws Exception {
+
+    public static void daftarPasien(Pasien pasien, int tanggal, int bulan, int tahun, Klinik klinik) throws Exception {
         if (cariAntrian(tanggal, bulan, tahun, klinik) == -1) {
             AntrianPasien.antrianPasien.get(cariAntrian(tanggal, bulan, tahun, klinik)).Mendaftar(pasien);
         } else {
@@ -110,6 +134,7 @@ public class AntrianPasien {
         antrian.setBulanAntrian(bulan);
         antrian.setTahunAntrian(tahun);
         antrian.setKlinik(klinik);
+
         if (cariAntrian(tanggal, bulan, tahun, klinik) == -1) {
             antrianPasien.add(antrian);
         } else {
@@ -124,6 +149,6 @@ public class AntrianPasien {
         for (int i = 0; i < daftarPasienAntri.size(); i++) {
             daftarPasienAntri.get(i).printInfo(); //mengeprint data pasien setiap index
         }
-   
 
-}}
+    }
+}
